f4400bff0d3164dd5ad1944ccb4214cb1ded9b28
phund <itghost90@gmail.com>
2015-12-22 13:16:23 +0700

    Add sond when run app force ground

---
+++ hook.sh
@@ -0,0 +1,4 @@
+mkdir -p .meteor/local/cordova-build/platforms/android/res/raw
+cp public/sound/alert.mp3 .meteor/local/cordova-build/platforms/android/res/raw/alert.mp3
+mkdir -p .meteor/local/cordova-build/platforms/android/res/drawable
+cp resources/icons/icon-22x22.png .meteor/local/cordova-build/platforms/android/res/drawable/pushicon.png
